6184fd4371107e210d5f7b4e0f58463bac3028d9
BEDRETTİN AKYÜZ <bedrettinakyuz@github.com>
2021-11-02 16:22:50 +0300

    x

--- src/main/java/com/springCRUDEx/SpringCrudExampleApplication.java
+++ src/main/java/com/springCRUDEx/SpringCrudExampleApplication.java
@@ -2,8 +2,9 @@ package com.springCRUDEx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringCrudExampleApplication {
 
 	public static void main(String[] args) {
