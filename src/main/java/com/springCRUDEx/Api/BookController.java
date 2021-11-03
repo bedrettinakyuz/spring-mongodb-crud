92ca0393efdbc017b97b970c878e261f92e739c1
BEDRETTİN AKYÜZ <bedrettinakyuz@github.com>
2021-11-03 08:08:39 +0300

    x

--- src/main/java/com/springCRUDEx/Api/BookController.java
+++ src/main/java/com/springCRUDEx/Api/BookController.java
@@ -2,6 +2,7 @@ package com.springCRUDEx.Api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.springCRUDEx.Model.Book;
 @RequestMapping("/book")
 public class BookController {
 
+	 @Autowired
 	private BookDal bookDal ;
 	
 	@PostMapping("/add")
