b50a86074f735dfbf55538ac5c41ca18de94066f
BEDRETTİN AKYÜZ <bedrettinakyuz@github.com>
2021-11-03 08:28:35 +0300

    x

--- src/main/java/com/springCRUDEx/Api/BookController.java
+++ src/main/java/com/springCRUDEx/Api/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +39,11 @@ public class BookController {
 		
 		return bookDal.getAll();
 	}
+	
+	@GetMapping("/get/{bookName}")
+	public List<Book> getByBookByName(@PathVariable String bookName){
+	return bookDal.getByBookName(bookName);
+	}
+
 	
 }
